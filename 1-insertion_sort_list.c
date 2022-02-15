07d0b58cca3baa5ba4d7505ef027ff5890efe8ad
MounaBenAli <benalimouna1986@gmail.com>
2022-02-15 09:26:31 +0100

    trial

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of int 
+ * @list : list to sort
+ * 
+ * Return - Nothing pick, compare, insert and print.
+ */
+
+
+void insertion_sort_list(listint_t **list)
+{
+ =
+}
